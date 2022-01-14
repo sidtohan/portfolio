cfeb5a4ab0b60349848b88dcb29102dbed55f263
sidtohan <siddhant.tohan@gmail.com>
2022-01-14 11:47:34 +0530

    Added the welcome section

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Welcome from "./components/Welcome";
+const App = () => {
+  return (
+    <>
+      <Welcome />
+    </>
+  );
+};
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Welcome = () => {
+  return (
+    <section id="welcome">
+      <h1>WELCOME</h1>
+      <div className="name">Siddhant Tohan</div>
+      <div className="welcome-skills">Fullstack Developer</div>
+    </section>
+  );
+};
+
+export default Welcome;
