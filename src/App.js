3f11e4d5be441e14bfaa25d2383802ceec434bf5
sidtohan <siddhant.tohan@gmail.com>
2022-01-14 19:17:26 +0530

    finished the about me section

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from "react";
 import Welcome from "./components/Welcome";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
-import pfp from "./profile-pic.png"
+import pfp from "./profile-pic.png";
 
 const App = () => {
   const navList = ["Home", "About Me", "Skills", "Achievements", "Projects"];
-  const desc = `I am a fullstack developer who is constantly looking to further my skills. I prefer to make minimalist designs for my websites that create an impact on the users.
-
-  I have experience in the MERN Stack, Python, C/C++, and version control systems(VCS) like Git . 
-  
-  I have interest in coding, watching anime, gaming and listening to music.`;
+  const desc = [
+    `I am a fullstack developer who is constantly looking to further my skills. 
+  I prefer to make minimalist designs for my websites that create an impact on the users.`,
+    `I have experience in the MERN Stack, Python, C/C++, and version control systems(VCS)
+  like Git.`,
+    `I have interest in coding, watching anime, gaming and listening to music.`,
+  ];
   return (
     <>
       <Header name={"sidtohan"} navList={navList} />
-      <Welcome name={"Siddhant Tohan"}/>
-      <AboutMe desc={desc} pfp={pfp}/>
+      <Welcome name={"Siddhant Tohan"} />
+      <AboutMe desc={desc} pfp={pfp} />
     </>
   );
 };
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,12 +5,16 @@ const AboutMe = ({ desc, pfp }) => {
     <section id="about-me">
       <h2>About Me</h2>
       <div className="about-me-info">
-        <p className="about-me-text">{desc}</p>
-        <img
-          src={pfp}
-          alt="Siddhant Tohan"
-          className="profile-pic"
-        />
+        <div className="about-me-text">
+          {desc.map((text, i) => {
+            return (
+              <>
+                <p key={i}>{text}</p> <br />
+              </>
+            );
+          })}
+        </div>
+        <img src={pfp} alt="Siddhant Tohan" className="profile-pic" />
       </div>
     </section>
   );
