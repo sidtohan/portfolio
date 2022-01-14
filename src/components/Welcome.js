301689484caa96e1ea2bced358bf19175acb5489
sidtohan <siddhant.tohan@gmail.com>
2022-01-14 11:59:41 +0530

    added some styling for welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Down_Chevron from "../assets/down-chevron.svg";
 const Welcome = () => {
   return (
-    <section id="welcome">
-      <h1>WELCOME</h1>
-      <div className="name">Siddhant Tohan</div>
-      <div className="welcome-skills">Fullstack Developer</div>
-    </section>
+    <>
+      <section id="welcome">
+        <h1>WELCOME</h1>
+        <div className="name">Siddhant Tohan</div>
+        <div className="welcome-skills">Fullstack Developer</div>
+      </section>
+      <img className="down-chevron" src={Down_Chevron} alt="down-chevron"></img>
+    </>
   );
 };
 
