3b2543549ca409ccca4ab62754a88b77b18f2733
sidtohan <siddhant.tohan@gmail.com>
2022-01-15 11:05:20 +0530

    made some changes to skill cards

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+const SkillCard = ({ skill }) => {
+  const [skillName, setSkillName] = useState("");
+  const showSkillName = () => setSkillName(skill.name);
+  const hideSkillName = () => setSkillName("");
+  return (
+    <div
+      className="skill-card"
+      onMouseEnter={showSkillName}
+      onMouseLeave={hideSkillName}
+    >
+      <img src={skill.icon} alt={skill.name} />
+      <div className="skill-name">{skillName}</div>
+    </div>
+  );
+};
 const Skills = ({ skills }) => {
   return (
     <section id="skills">
       <h2>Skills</h2>
       <div className="skill-card-holder">
         {skills.map((skill) => (
-          <div key={skill.name} className="skill-card">
-            <img src={skill.icon} alt={skill.name}/>
-          </div>
+          <SkillCard skill={skill} key={skill.name} />
         ))}
       </div>
     </section>
