3d75661f5380afaba68991f15c6f124899f0d90f
sidtohan <siddhant.tohan@gmail.com>
2022-01-22 17:20:26 +0530

    added logic for burger menu and close button also

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Icon } from "@iconify/react";
 const Header = ({ name, navList, headerRef }) => {
+  const navListRef = useRef(null);
+  const closeButtonRef = useRef(null);
+  const burgerMenuRef = useRef(null);
+
+  useEffect(() => {
+    const burgerButton = burgerMenuRef.current;
+    burgerButton.addEventListener("click", () => {
+      navListRef.current.classList.add("show");
+      closeButtonRef.current.classList.add("show");
+    });
+
+    const closeButton = closeButtonRef.current;
+    closeButton.addEventListener("click", () => {
+      navListRef.current.classList.remove("show");
+      closeButton.classList.remove("show");
+    });
+
+    const navList = navListRef.current;
+    navList.childNodes.forEach((item) => {
+      item.addEventListener("click", () => {
+        navList.classList.remove("show");
+        closeButton.classList.remove("show");
+      });
+    });
+  }, []);
   return (
     <header ref={headerRef}>
       <h1 className="header-name">{name}</h1>
       <nav>
-        <ul className="nav-list">
+        <ul className="nav-list" ref={navListRef}>
           {navList.map((item, i) => (
             <li
               key={i}
@@ -26,11 +51,16 @@ const Header = ({ name, navList, headerRef }) => {
             </li>
           ))}
         </ul>
-        <Icon
-          icon={"charm:menu-hamburger"}
-          color="#fff"
-          className="burger-menu"
-        />
+        <button className="burger-button" ref={burgerMenuRef}>
+          <Icon
+            icon={"charm:menu-hamburger"}
+            color="#fff"
+            className="burger-menu"
+          />
+        </button>
+        <button className="close-menu-button" ref={closeButtonRef}>
+          <Icon icon={"charm:cross"} color="#fff" className="close-menu" />
+        </button>
       </nav>
     </header>
   );
